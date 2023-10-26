8d2202b169740d8eeb24adaf3769aa5f5d407c1e
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-10-26 15:04:37 +0900

    3.2.1 Todo 리스트 : material ui를 이용한 디자인

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import './App.css';
 import Todo from "./Todo";
 import React from "react";
+import { Paper, List } from "@material-ui/core";
 
 class  App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            item: [
+            items: [
                 {id: 0, title: "Hello World 1", done: true},
                 {id: 1, title: "Hello World 2", done: false}
             ],
         }
     }
-  render() {
-        var todoItems = this.state.item.map((item, idx) => (
-            <Todo item={item} key={item.id} />
-        ));
-        return (
-            <div className="App">
-                {todoItems}
-            </div>
+
+    render() {
+        var todoItems = this.state.items.length > 0 && (
+            <Paper style={{margin: 16}}>
+                <List>
+                    {this.state.items.map((item, idx) => (
+                        <Todo item={item} key={item.id}/>
+                    ))}
+                </List>
+            </Paper>
         );
+        return <div className="App">{todoItems}</div>;
     }
 }
 
